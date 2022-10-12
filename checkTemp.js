5c72fa2ca40d448c3547eed41a80dd1fe521e218
Angelina <115346254+AngelinaPjatkovskaja@users.noreply.github.com>
2022-10-12 11:50:04 +0300

    Update checkTemp.js

---
+++ checkTemp.js
@@ -0,0 +1,14 @@
+function checkTemp(temperatuur) {
+    if (temperatuur < 0) {
+        return 
+    }
+    else if (temperatuur <= 20) {
+        return -1
+    }
+    else if (temperatuur <= 40) {
+        return 0
+    }
+    else if (temperatuur <= 60) {
+        return 1
+    }    
+}
